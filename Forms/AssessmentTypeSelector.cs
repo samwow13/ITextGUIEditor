7b3e39d098800386bcd905f8a03375bf34d18341
Samuel Wetzel <samwow13@yahoo.com>
2025-02-27 12:22:07 -0800

    removed un-necessary code

--- Forms/AssessmentTypeSelector.cs
+++ Forms/AssessmentTypeSelector.cs
@@ -8,8 +8,13 @@ using iTextDesignerWithGUI.Services;
 
 namespace iTextDesignerWithGUI.Forms
 {
-    public partial class AssessmentTypeSelector : Form
+    public class AssessmentTypeSelector : Form
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
         public AssessmentTypeWrapper SelectedTypeWrapper { get; private set; }
         public string SelectedBuiltInType => SelectedTypeWrapper?.IsBuiltIn == true ? SelectedTypeWrapper.BuiltInType : null;
         public bool WasCancelled { get; private set; } = true;
@@ -18,6 +23,35 @@ namespace iTextDesignerWithGUI.Forms
         {
             InitializeComponent();
             InitializeForm();
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         }
 
         private void InitializeForm()
